64cf70e8c2775113f00ffa3e1bd47b8c96d69cb8
Quentin <Q.Pavie@etudiant.ecolemelies.fr>
2021-12-14 15:01:10 +0100

    Timer Finish
    add the timer with a coroutine

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,9 +5,29 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] TextMeshPro timerText;
+    public float currentTime;
+    public float startingTime;
+
+    [SerializeField] TextMeshProUGUI timerText;
+
     void Start()
     {
-        
+        currentTime = startingTime;
+        StartCoroutine(RunTimer());
+    }
+
+    private IEnumerator RunTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            currentTime -= 1;
+            timerText.text = currentTime.ToString("0");
+
+            if (currentTime == 0)
+            {
+                currentTime = startingTime;
+            }
+        }
     }
 }
