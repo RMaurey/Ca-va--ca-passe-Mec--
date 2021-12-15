b7002c984bd320b207c8e15af9acda30d2a18689
Valentin <valentinbf94@gmail.com>
2021-12-15 14:55:32 +0100

    Ajouts de toutes les photos

--- Assets/InputActions/Controls.cs
+++ Assets/InputActions/Controls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/InputActions/Controls.inputactions'
 
 using System;
 using System.Collections;
