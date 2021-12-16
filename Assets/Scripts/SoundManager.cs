e9434226a3c7d4d9f8eb6d0dc5628e8dcec75137
Maurey <r.maurey@etudiant.ecolemelies.fr>
2021-12-16 16:52:13 +0100

    Sound
    Association voix aux scenes

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class SoundManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+        s.source.Play();
+    }
+}
